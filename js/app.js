99402467503df4631cd7d61bf21cf367bf14f33e
mse2321 <mse233@gmail.com>
2015-07-20 23:05:05 -0400

    getting ready for session with Agustin

---
+++ js/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+	function questionAnswered() {
+		//determines the question progress
+		if () {
+
+		} else {
+			
+		}
+	}
+
+	function rightAnswer() {
+		//determines the question is correct
+		if () {
+			//do this
+		} else {
+			//do this
+		}
+	}
+
+	function showScore() {
+		//shows the user there score
+	}
+
+	function questions() {
+		var question1 = "this";
+		var question2 = "this";
+		var question3 = "this";
+		var question4 = "this";
+		var question5 = "this";
+
+		var allQuestions = [question1, question2, question3, question4, question5];
+	}
+
+	function answers() {
+		var question1Answers = "this, this, this, this";
+		var question2Answers = "this, this, this, this";
+		var question3Answers = "this, this, this, this";
+		var question4Answers = "this, this, this, this";
+		var question5Answers = "this, this, this, this";
+
+		var allAnswers = [question1Answers, question2Answers, question3Answers, question4Answers, question5Answers];
+	}
+
+});
