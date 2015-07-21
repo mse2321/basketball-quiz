f167fe7906a1a79bac21c8107cf4c9af09d8b729
mse2321 <mse233@gmail.com>
2015-07-21 18:37:30 -0400

    removed all previous logic

--- js/app.js
+++ js/app.js
@@ -27,24 +27,6 @@ $(document).ready(function () {
 		answers: this, this, this, this
 	}
 
-	function questions() {
-		var question1 = "this";
-		var question2 = "this";
-		var question3 = "this";
-		var question4 = "this";
-		var question5 = "this";
-
-		var allQuestions = [question1, question2, question3, question4, question5];
-	}
-
-	function answers() {
-		var question1Answers = "this, this, this, this";
-		var question2Answers = "this, this, this, this";
-		var question3Answers = "this, this, this, this";
-		var question4Answers = "this, this, this, this";
-		var question5Answers = "this, this, this, this";
-
-		var allAnswers = [question1Answers, question2Answers, question3Answers, question4Answers, question5Answers];
-	}
+	var question2 = Object.create(questions);
 
 });
