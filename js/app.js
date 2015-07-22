7a900c7bbbcaf7e2107214e8fde665afcc162b66
mse2321 <mse233@gmail.com>
2015-07-21 21:32:13 -0400

    laid out most of the js foundation

--- js/app.js
+++ js/app.js
@@ -20,13 +20,39 @@ $(document).ready(function () {
 
 	function showScore() {
 		//shows the user there score
+		var finalScore = score + points;
 	}
+
+	var quiz = [question1, question2, question3, question4, question5];
 
 	var questions = {
-		question: blah blah,
-		answers: this, this, this, this
+		question: text,
+		answers: ["this", "this", "this", "this"]
 	}
 
+	var question1 = Object.create(questions);
+
+	question1.question = "";
+	question1.answer = ["this", "this", "this", "this"];
+
 	var question2 = Object.create(questions);
+
+	question2.question = "";
+	question2.answer = ["this", "this", "this", "this"];
+
+	var question3 = Object.create(questions);
+
+	question3.question = "";
+	question3.answer = ["this", "this", "this", "this"];
+
+	var question4 = Object.create(questions);
+
+	question4.question = "";
+	question4.answer = ["this", "this", "this", "this"];
+
+	var question5 = Object.create(questions);
+
+	question5.question = "";
+	question5.answer = ["this", "this", "this", "this"];
 
 });
