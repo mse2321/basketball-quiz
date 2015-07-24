f59afa10aedd8af898ed48f3bf12324b6efe2044
mse2321 <mse233@gmail.com>
2015-07-23 21:33:54 -0400

    cleaning up js and adding questions

--- js/app.js
+++ js/app.js
@@ -1,17 +1,44 @@
 $(document).ready(function () {
 
-	function questionAnswered() {
-		//determines the question progress
-		if () {
+	var questions = {
+
+		question: "text",
+		answers: ["this", "this", "this", "this"],
+		correctAnswer: answers[0]
+	}
+
+	// Question Handler
+	$("#question").html(showQuestion);
+
+	// Answers Handler
+	$("#answerA").html(showAnswers);
+
+	// User Answer Handler
+	$("button").click(rightAnswerCheck);
+
+
+	function userAnswer() {
+		var answer = something;
+
+		if (answer === something) {
 
 		} else {
 			
 		}
 	}
 
-	function rightAnswer() {
+	function questionAnswered() {
+		//determines the question progress
+		if (answer !== NULL) {
+
+		} else {
+			
+		}
+	}
+
+	function rightAnswerCheck() {
 		//determines the question is correct
-		if () {
+		if (answer === correctAnswer) {
 			//do this
 		} else {
 			//do this
@@ -20,49 +47,62 @@ $(document).ready(function () {
 
 	function showScore() {
 		//shows the user there score
-		var finalScore = score + points;
+		//var finalScore = score + points;
+		var finalScore = 100;
+		$("#actualScore").html(finalScore);
 	}
 
-	var quiz = [question1, question2, question3, question4, question5];
+	function showQuestion() {
+		var quiz = [question1.question, question2.question, question3.question, question4.question, question5.question];
+		var nextQuestion = quiz[0];
 
-	var questions = {
-		question: text,
-		answers: ["this", "this", "this", "this"]
+		return nextQuestion;
+	}
+
+	function showAnswers() {
+		var nextQuestionAnswers = [question1.answers, question2.answers, question3.answers, question4.answers, question5.answers];
+
+		return nextQuestionAnswers;
 	}
 
 	function questionOne () {
 		var question1 = Object.create(questions);
 
-		question1.question = "";
-		question1.answer = ["this1", "this1", "this1", "this1"];
+		question1.question = "Which of the teams below has not won more than 2 NBA Championship?",
+		question1.answers = ["Boston Celtics", "Los Angeles Lakers", "Orlando Magic", "Miami Heat"];
+		question1.correctAnswer = question1.answers[2]
 	}
 
 	function questionTwo () {
 		var question2 = Object.create(questions);
 
-		question2.question = "";
-		question2.answer = ["this2", "this2", "this2", "this2"];
+		question2.question = "",
+		question2.answers = ["this2", "this2", "this2", "this2"];
+		question2.correctAnswer = question2.answer[0]
 	}
 
 	function questionThree () {
 		var question3 = Object.create(questions);
 
-		question3.question = "";
-		question3.answer = ["this3", "this3", "this3", "this3"];
+		question3.question = "",
+		question3.answers = ["this3", "this3", "this3", "this3"];
+		question3.correctAnswer = question3.answer[0]
 	}
 
 	function questionFour () {
 		var question4 = Object.create(questions);
 
-		question4.question = "";
-		question4.answer = ["this4", "this4", "this4", "this4"];
+		question4.question = "",
+		question4.answers = ["this4", "this4", "this4", "this4"];
+		question4.correctAnswer = question4.answer[0]
 	}
 
 	function questionFive () {
 		var question5 = Object.create(questions);
 
-		question5.question = "";
-		question5.answer = ["this5", "this5", "this5", "this5"];
+		question5.question = "",
+		question5.answers = ["this5", "this5", "this5", "this5"];
+		question5.correctAnswer = question5.answer[0]
 	}
 
 	function progressMade() {
