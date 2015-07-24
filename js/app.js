d23b14e770261fc60c329fdd69911c7caf64c8d9
mse2321 <mse233@gmail.com>
2015-07-23 21:59:35 -0400

    moved all questions under one function

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
 	var questions = {
 
 		question: "text",
-		answers: ["this", "this", "this", "this"],
-		correctAnswer: answers[0]
+		answers: [],
+		correctAnswer: answers,
 	}
 
 	// Question Handler
@@ -17,26 +17,9 @@ $(document).ready(function () {
 	$("button").click(rightAnswerCheck);
 
 
-	function userAnswer() {
+	function rightAnswerCheck() {
 		var answer = something;
 
-		if (answer === something) {
-
-		} else {
-			
-		}
-	}
-
-	function questionAnswered() {
-		//determines the question progress
-		if (answer !== NULL) {
-
-		} else {
-			
-		}
-	}
-
-	function rightAnswerCheck() {
 		//determines the question is correct
 		if (answer === correctAnswer) {
 			//do this
@@ -53,60 +36,76 @@ $(document).ready(function () {
 	}
 
 	function showQuestion() {
-		var quiz = [question1.question, question2.question, question3.question, question4.question, question5.question];
+
+		var quiz = [question1, question2, question3, question4, question5];
 		var nextQuestion = quiz[0];
+
+		function questionOne () {
+			var question1 = Object.create(questions);
+
+			question1.question = "Which of the teams below has not won more than 2 NBA Championship?";
+			question1.answers = ["Boston Celtics", "Los Angeles Lakers", "Orlando Magic", "Miami Heat"];
+			question1.correctAnswer = question1.answers[2];
+
+			return question1;
+
+			console.log(question1.question);
+		}
+
+		function questionTwo () {
+			var question2 = Object.create(questions);
+
+			question2.question = "";
+			question2.answers = ["this2", "this2", "this2", "this2"];
+			question2.correctAnswer = question2.answer[0];
+
+			return question2;
+
+			console.log(question2);
+		}
+
+		function questionThree () {
+			var question3 = Object.create(questions);
+
+			question3.question = "";
+			question3.answers = ["this3", "this3", "this3", "this3"];
+			question3.correctAnswer = question3.answer[0];
+
+			return question3;
+
+			console.log(question3);
+		}
+
+		function questionFour () {
+			var question4 = Object.create(questions);
+
+			question4.question = "";
+			question4.answers = ["this4", "this4", "this4", "this4"];
+			question4.correctAnswer = question4.answer[0];
+
+			return question4;
+
+			console.log(question4);
+		}
+
+		function questionFive () {
+			var question5 = Object.create(questions);
+
+			question5.question = "";
+			question5.answers = ["this5", "this5", "this5", "this5"];
+			question5.correctAnswer = question5.answer[0];
+
+			return question5;
+
+			console.log(question5);
+		}
 
 		return nextQuestion;
 	}
-
-	function showAnswers() {
-		var nextQuestionAnswers = [question1.answers, question2.answers, question3.answers, question4.answers, question5.answers];
-
-		return nextQuestionAnswers;
-	}
-
-	function questionOne () {
-		var question1 = Object.create(questions);
-
-		question1.question = "Which of the teams below has not won more than 2 NBA Championship?",
-		question1.answers = ["Boston Celtics", "Los Angeles Lakers", "Orlando Magic", "Miami Heat"];
-		question1.correctAnswer = question1.answers[2]
-	}
-
-	function questionTwo () {
-		var question2 = Object.create(questions);
-
-		question2.question = "",
-		question2.answers = ["this2", "this2", "this2", "this2"];
-		question2.correctAnswer = question2.answer[0]
-	}
-
-	function questionThree () {
-		var question3 = Object.create(questions);
-
-		question3.question = "",
-		question3.answers = ["this3", "this3", "this3", "this3"];
-		question3.correctAnswer = question3.answer[0]
-	}
-
-	function questionFour () {
-		var question4 = Object.create(questions);
-
-		question4.question = "",
-		question4.answers = ["this4", "this4", "this4", "this4"];
-		question4.correctAnswer = question4.answer[0]
-	}
-
-	function questionFive () {
-		var question5 = Object.create(questions);
-
-		question5.question = "",
-		question5.answers = ["this5", "this5", "this5", "this5"];
-		question5.correctAnswer = question5.answer[0]
-	}
+	
 
 	function progressMade() {
-		progress = "something"
+		progress = numberOfAnswers;
 
 		if(progress === 0) {
 			alert("please answer a question.")
