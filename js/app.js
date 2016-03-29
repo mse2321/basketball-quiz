7b1d35956f8a7930db44f6afa4d06df2a0789bb5
mse2321 <mse233@gmail.com>
2016-03-29 16:56:59 -0400

    almost done with styling

--- js/app.js
+++ js/app.js
@@ -28,11 +28,11 @@ $(document).ready(function () {
 
 	function showQuestion() {
 	  	var questions = [
-	    {question:'Which of this teams has NOT won an NBA Champion?', correct: 'A - Orlando Magic', answers:['A - Orlando Magic', 'B - Boston Celtics', 'C - Houston Rockets', 'D - Dallas Mavericks']},
-	    {question:'What NBA player was the sillouette for their logo?', correct: 'C - Jerry West', answers:['A - Wilt Chamberlain', 'B - Michael Jordan', 'C - Jerry West', 'D - Julius Erving']},
-	    {question:'What company makes the official NBA basketball?', correct: 'B - Spalding', answers:['A - Champion', 'B - Spalding', 'C - Nike', 'D - Franklin Sports']},
-	    {question:'What NBA team plays their games in Chicago?', correct: 'C - Bulls', answers:['A - Kings', 'B - Bucks', 'C - Bulls', 'D - Pistons']},
-	    {question:'Which team has won the most NBA Championships?', correct: 'D - Boston Celtics', answers:['A - Los Angeles Lakers', 'B - San Antonio Spurs', 'C - New York Knicks', 'D - Boston Celtics']}
+		    {question:'Which of this teams has NOT won an NBA Champion?', correct: 'A - Orlando Magic', answers:['A - Orlando Magic', 'B - Boston Celtics', 'C - Houston Rockets', 'D - Dallas Mavericks']},
+		    {question:'What NBA player was the sillouette for their logo?', correct: 'C - Jerry West', answers:['A - Wilt Chamberlain', 'B - Michael Jordan', 'C - Jerry West', 'D - Julius Erving']},
+		    {question:'What company makes the official NBA basketball?', correct: 'B - Spalding', answers:['A - Champion', 'B - Spalding', 'C - Nike', 'D - Franklin Sports']},
+		    {question:'What NBA team plays their games in Chicago?', correct: 'C - Bulls', answers:['A - Kings', 'B - Bucks', 'C - Bulls', 'D - Pistons']},
+		    {question:'Which team has won the most NBA Championships?', correct: 'D - Boston Celtics', answers:['A - Los Angeles Lakers', 'B - San Antonio Spurs', 'C - New York Knicks', 'D - Boston Celtics']}
 	    ];
 	    
 	    var currentQuestion = 0;
