49e5321b462d27906f933167fff01396a419bea1
Marcus Eubanks <mse233@gmail.com>
2021-05-09 23:58:22 -0400

    initial commit for complete refactor into react app

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+    return (
+        <header>
+		    <h1>Basketball Quiz</h1>
+	    </header>
+    );
+};
+  
+export default Header;
+  
---
+++ src/components/views/Intro.jsx
@@ -0,0 +1,14 @@
+
+const Intro = () => {
+  return (
+    <div id="intro">
+        <div class="container">
+            <h2>Get ready to take the quiz!</h2>
+            <p>See how much you know about the NBA. Click below to see how you do.</p>
+            <a href="#/quiz"><button id="quiz-start">Start</button></a>
+        </div>
+    </div>
+  );
+}
+
+export default Intro;
